af41ae95c32c3bbc50f38f2278cc1a6a40cc3b24
Lucas Malaman de Oliveira <lucasm@ciandt.com>
2019-06-12 19:32:12 -0300

    axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <HelloWorld />
+  <v-data-table :headers="headers" :items="residences" class="elevation-1">
+    <template slot-scope="{ item }">
+      <td>{{ item.title }}</td>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
+export default {
+  data: () => ({
+    headers: [
+      { text: "Título" }
+    ],
+    residences: []
+  }),
+  methods: {
+    async load() {
+      this.residences = (await this.$http.get('http://localhost:9090/residence')).data
     }
+  },
+  mounted() {
+    this.load()
   }
+};
 </script>
