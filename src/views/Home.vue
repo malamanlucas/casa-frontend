9b93c19f051cfb09de6dee0f7be562d0d8168c38
Lucas Malaman de Oliveira <lucasm@ciandt.com>
2019-06-14 19:57:29 -0300

    x

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,41 +6,36 @@
       <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
     </v-card-title>
 
+    <v-container fluid>
+      <v-layout row justify-center>
+        <v-flex xs2 v-for="option in sponsors" :key="option">
+          <v-checkbox v-model="sponsorsSelecteds" :label="option" :value="option"></v-checkbox>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
     <v-data-table
       :search="search"
       :expand="true"
       :headers="headers"
-      :items="residences"
+      :items="residencesFiltered"
       item-key="id"
       class="elevation-1"
     >
-      <!-- <template #headers="{ headers }">
-        <tr>
-          <th
-            v-for="header in headers"
-            :key="header.text"
-            :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
-            @click="changeSort(header.value)"
-          >
-            <v-icon small>arrow_upward</v-icon>
-            {{ header.text }}
-          </th>
-        </tr>
-      </template> -->
-
       <template v-slot:no-data>
         <v-alert :value="true" color="error" icon="warning">Nenhuma residência cadastrada no banco</v-alert>
       </template>
 
       <template #items="props">
         <tr @click="props.expanded = !props.expanded">
+          <!-- <td>{{ props.item.type }}</td> -->
           <td>{{ props.item.address.district }}</td>
           <td>{{ props.item.title }}</td>
           <td>{{ props.item.address.addressFully }}, {{ props.item.address.zipCode | cep }}</td>
           <!-- <td>{{ props.item.monthPrice.value | currency }}</td>
         <td>{{ props.item.monthPrice.condominium | currency }}</td>
           <td>{{ props.item.monthPrice.iptu | currency }}</td>-->
-          <td>{{ props.item.monthPrice.totalAmount | currency }}</td>
+          <td>{{ props.item.monthPrice.calcTotalAmount | currency }}</td>
         </tr>
       </template>
 
@@ -76,28 +71,41 @@
 </template>
 
 <script>
+import { isEmpty }from 'lodash'
+
 export default {
   data: () => ({
+    sponsors: [],
+    sponsorsSelecteds: [],
     pagination: {
       sortBy: ""
     },
     search: "",
     expand: false,
     headers: [
+      // { text: "Tipo", value: "type" },
       { text: "Bairro", value: "address.district" },
       { text: "Título", value: "title" },
       { text: "Endereço", value: "street" },
       // { text: "Preço", value: "monthPrice.value" },
       // { text: "Condomínio", value: "monthPrice.condominium" },
       // { text: "IPTU", value: "monthPrice.iptu" },
-      { text: "Total (preço + cond)", value: "monthPrice.totalAmount" }
+      { text: "Total (preço + cond)", value: "monthPrice.calcTotalAmount" }
     ],
     residences: []
   }),
+  computed: {
+    residencesFiltered() {
+      if (isEmpty(this.sponsorsSelecteds)) {
+        return this.residences
+      }
+      return this.residences.filter(r => this.sponsorsSelecteds.some(s => s == r.type))
+    }
+  },
   methods: {
     async load() {
-      const response = await this.$axios.get("/residence");
-      this.residences = response.data;
+      this.residences = (await this.$axios.get("/residence")).data;
+      this.sponsors = (await this.$axios.get("/sponsors")).data;
     },
     changeSort(column) {
       if (this.pagination.sortBy === column) {
@@ -109,7 +117,7 @@ export default {
     }
   },
   mounted() {
-    this.load();
+    this.load()
   }
 };
 </script>
